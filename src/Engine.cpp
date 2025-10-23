7e49186b27122aa9761f974bb623f913ef41588a
TheRealFirst <88617589+TheRealFirst@users.noreply.github.com>
2025-10-23 16:27:07 +0200

    first cmake attempt

---
+++ src/Engine.cpp
@@ -0,0 +1,8 @@
+#include "Engine.h"
+
+void Engine::Init()
+{
+	initialize_logging();
+
+	LOG_DEBUG("Hello World!");
+}
---
+++ src/Engine.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <tinyLog/Log.h>
+
+namespace PSB 
+{
+	class Engine()
+	{
+		void Init();
+	}
+}
